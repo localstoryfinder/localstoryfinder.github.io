24f0fe6b5e71e0272699691ea22ce6756180a698
Sana Dadani <sanadadani15@gmail.com>
2025-02-22 04:59:09 +0000

    test push

---
+++ docs/waypoints.js
@@ -0,0 +1,6 @@
+var waypoint = new Waypoint({
+  element: document.getElementById('waypoint'),
+  handler: function() {
+   console.log('arrived at waypoints');
+   //notify('Basic waypoint triggered')
+}});
