0eecb50ce18fddb52f163edbf2f563f4df7f4eac
enkhjinm <126828903+enkhjinm@users.noreply.github.com>
2025-02-24 15:09:16 -0800

    Delete docs/waypoints.js

--- docs/waypoints.js
+++
@@ -1,6 +0,0 @@
-var waypoint = new Waypoint({
-  element: document.getElementById('waypoint'),
-  handler: function() {
-   console.log('arrived at waypoints');
-   //notify('Basic waypoint triggered')
-}});
